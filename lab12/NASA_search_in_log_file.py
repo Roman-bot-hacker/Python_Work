04e0470a74636d928a0b0bd11d23de5679294ef9
Roman Kruk <roman1writer@gmail.com>
2018-05-08 12:41:51 +0300

    Fixed pattern

--- lab12/NASA_search_in_log_file.py
+++ lab12/NASA_search_in_log_file.py
@@ -1,4 +1,3 @@
-import timeit
 import zipfile
 import re
 
@@ -7,9 +6,9 @@ def NASA_search_in_log_file():
     zip_file = zipfile.ZipFile('access_log_Jul95.zip')
     file = zip_file.open('access_log_Jul95')
     for line in file:
-        if re.search('01/Jul/1995:00:1[0-9].*GET.*NASA', str(line)):
+        if re.search('01/Jul/1995:00:1[0-9].*GET.*[^\[]NASA', str(line)):
             count+=1
-    print(count-1)
+    print(count)
     zip_file.close()
 
 
